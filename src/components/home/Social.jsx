6b41283e51c0b665afead99ffa6abc00ab190d93
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-12-24 23:04:24 -0300

    home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/matheus-trinca-4740b11b0"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+      <a
+        href="https://api.whatsapp.com/send?phone=5514999014372"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-whatsapp"></i>
+      </a>
+      <a
+        href="https://github.com/MatheusTrinca"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
